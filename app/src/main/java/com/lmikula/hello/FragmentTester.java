a096a8c852c06abc27603bca0aba876e08ae82de
LMikula-GameDev <lindsay.mikula@gmail.com>
2014-09-29 20:58:15 -0400

    Tutorial end; somewhat broken version of fragments (works, but not done correctly)

--- app/src/main/java/com/lmikula/hello/FragmentTester.java
+++ app/src/main/java/com/lmikula/hello/FragmentTester.java
@@ -1,7 +1,10 @@
 package com.lmikula.hello;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,13 +14,21 @@ import android.widget.Button;
 public class FragmentTester extends Activity {
 
     Button b;
+    public static Fragment FunkyFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_tester);
 
+//        if (FunkyFragment == null)
+            FunkyFragment = new ColourFragment();
+
         b = (Button) findViewById(R.id.fragmentButton1);
         b.setText("Switch fragments");
+        FragmentManager fragMngr = getFragmentManager();
+        fragMngr.beginTransaction()
+        .add(R.id.LayoutLow,FunkyFragment)
+                .commit();
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/lmikula/hello/FragmentTester2.java
+++ app/src/main/java/com/lmikula/hello/FragmentTester2.java
@@ -1,6 +1,8 @@
 package com.lmikula.hello;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,17 +14,25 @@ import android.widget.Button;
 public class FragmentTester2 extends Activity {
 
     Button b;
+    public static Fragment FunkyFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_tester2);
 
+//        if (FunkyFragment == null)
+        FunkyFragment = new ColourFragment();
+
         b = (Button) findViewById(R.id.fragmentButton2);
         b.setText("Switch fragments");
+        FragmentManager fragMngr = getFragmentManager();
+        fragMngr.beginTransaction()
+                .add(R.id.LayoutHigh,FunkyFragment)
+                .commit();
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), FragmentTester.class));
+                startActivity(new Intent(getApplicationContext(),FragmentTester.class));
             }
         });
     }
